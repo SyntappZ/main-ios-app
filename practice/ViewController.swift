0c6ed17ee81f41f3d22afb384081edcacfea8327
syntappz <doddsanater87@gmail.com>
2019-11-20 14:23:26 +0000

    first commit

---
+++ practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by CN0104 on 20/11/2019.
+//  Copyright © 2019 the ringers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
